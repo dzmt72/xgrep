84d1fecee81dfa1cbf57ebc98cbe4f03c5a6ea95
dzmt72 <seku58@tutamail.com>
2025-01-30 22:02:37 +0300

    add parsing file function & clap usage for terminal args

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
-use std::{fs::File, io::BufReader};
+use std::{fs::File, io::{BufRead, BufReader}};
 use regex::Regex;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg()]
+    regex: String,
+
+    #[arg()]
+    path: String
+}
 
 fn parse_file_re(reader: BufReader<&File>, re: Regex) -> Vec<(usize, String)> {
-    let result: Vec<(usize, String)> = Vec::new();
+    let mut result: Vec<(usize, String)> = Vec::new();
 
-    unimplemented!()
+    for (i, line) in reader.lines().enumerate() {
+        if let Ok(l) = line {
+            if re.is_match(&l) {
+                result.push((i + 1, l));
+            }
+        } else {
+            eprintln!("error reading line {}: {}", i, line.unwrap_err());
+        }
+    }
+
+    result 
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let file = File::open("demo")?;
+    let args = Args::parse();
 
-    let re = Regex::new(r"hey").unwrap();
+    let file = File::open(&args.path)?;
+
+    let re = Regex::new(&args.regex).unwrap();
     let reader = BufReader::new(&file);
 
     let result: Vec<(usize, String)> = parse_file_re(reader, re);
 
-    dbg!(result);
+    for (i, l) in result {
+        println!("{}\t{}\t{}", i, l, &args.path);
+    }
 
     Ok(())
 }
